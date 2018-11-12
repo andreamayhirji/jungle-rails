5367d9caa3c447def5ce0577ec56e78cf182c88d
Andrea <andreamayhirji@gmail.com>
2018-11-12 22:32:06 +0000

    Adds successful spec for the products.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,33 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+    category = Category.create(name: "Anything")
+    subject { Product.new(name:"Anything", description:"Lore ipsum", image:"Anything.jpeg", price_cents: "Anything".to_i, quantity: "Anything".to_i, category: category) }
+
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a name' do 
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+    
+    it 'it is not valid without a price' do
+      subject.price_cents = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'it is not valid without a quanitity' do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'it is not valid without a category' do
+      subject.category_id = nil
+      expect(subject).to_not be_valid
+    end
+
   end
 
 end
